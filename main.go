2d2b2f1711e0b0a219d2247836ebc2cbbb3a3911
Zewasik <shumahershik@gmail.com>
2023-05-14 20:40:23 +0300

    move download functionality from main to package

--- main.go
+++ main.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"io/fs"
 	"log"
-	"net/http"
 	"os"
 	"path"
-	"strings"
 	"time"
+	"wget/packages/downloader"
 	flag_parser "wget/packages/flag-parser"
 )
 
-const progressBarWidth = 40
 const outputFormat = "Content size: %d bytes [~ %.2f Mb]\nSaving file to: %s\nFinished at %s\n"
 
 func main() {
@@ -40,23 +37,7 @@ func main() {
 		log.Fatalf("Error: %v", err)
 	}
 
-	storage.GetFlag("B")
-
 	fmt.Printf("Start at %s\n", formatTime(time.Now()))
-
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Println("Error fetching URL:", err)
-		return
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error fetching URL: recieved status code", resp.StatusCode)
-	} else {
-		fmt.Println("Sending request, awaiting response... status 200 OK")
-	}
 
 	fileName := ""
 	dirPath := ""
@@ -77,54 +58,12 @@ func main() {
 		}
 	}
 
-	file, err := os.Create(dirPath + fileName)
+	currentSize, err := downloader.Download(url, fileName, dirPath)
 	if err != nil {
-		fmt.Printf("Error creating file %v", err)
-		return
+		log.Fatalln(err)
 	}
 
-	defer file.Close()
-
-	currentSize := int64(0)
-
-	if resp.ContentLength > 0 {
-		go oneSecondTick(&currentSize, resp)
-	}
-
-	_, err = io.Copy(file, io.TeeReader(resp.Body, &progressReader{&currentSize}))
-	if err != nil {
-		fmt.Printf("Cannot download file %v", err)
-		return
-	}
-
-	if resp.ContentLength > 0 {
-		fmt.Printf("\r[%s] %.2f%% of %d bytes\n", progressBar(currentSize, resp.ContentLength), float64(currentSize)/float64(resp.ContentLength)*100, resp.ContentLength)
-	}
 	fmt.Printf(outputFormat, currentSize, bytesToMb(currentSize), fileName, formatTime(time.Now()))
-}
-
-func oneSecondTick(size *int64, r *http.Response) {
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		fmt.Printf("\r[%s] %.2f%% of %d bytes", progressBar(*size, r.ContentLength), float64(*size)/float64(r.ContentLength)*100, r.ContentLength)
-	}
-}
-
-type progressReader struct {
-	currentSize *int64
-}
-
-func (pr *progressReader) Write(p []byte) (n int, err error) {
-	n = len(p)
-	*pr.currentSize += int64(n)
-	return
-}
-
-func progressBar(currentSize, totalSize int64) string {
-	progress := int(float64(currentSize) / float64(totalSize) * float64(progressBarWidth))
-	return strings.Repeat("=", progress) + strings.Repeat("-", progressBarWidth-progress)
 }
 
 func formatTime(t time.Time) string {
---
+++ packages/downloader/downloader.go
@@ -0,0 +1,72 @@
+package downloader
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+const progressBarWidth = 40
+
+func Download(url, fileName, dirPath string) (int64, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return 0, err
+	}
+
+	if resp.StatusCode == http.StatusOK {
+		fmt.Println("Sending request, awaiting response... status 200 OK")
+	} else {
+		fmt.Printf("Error downloading %s: recieved status code %d", url, resp.StatusCode)
+	}
+
+	file, err := os.Create(dirPath + fileName)
+	if err != nil {
+		return 0, err
+	}
+
+	defer file.Close()
+
+	currentSize := int64(0)
+
+	if resp.ContentLength > 0 {
+		go oneSecondTick(&currentSize, resp)
+	}
+
+	_, err = io.Copy(file, io.TeeReader(resp.Body, &progressReader{&currentSize}))
+	if err != nil {
+		return 0, err
+	}
+
+	if resp.ContentLength > 0 {
+		fmt.Printf("\r[%s] %.2f%% of %d bytes\n", progressBar(currentSize, resp.ContentLength), float64(currentSize)/float64(resp.ContentLength)*100, resp.ContentLength)
+	}
+
+	return currentSize, nil
+}
+
+type progressReader struct {
+	currentSize *int64
+}
+
+func (pr *progressReader) Write(p []byte) (n int, err error) {
+	n = len(p)
+	*pr.currentSize += int64(n)
+	return
+}
+
+func progressBar(currentSize, totalSize int64) string {
+	progress := int(float64(currentSize) / float64(totalSize) * float64(progressBarWidth))
+	return strings.Repeat("=", progress) + strings.Repeat("-", progressBarWidth-progress)
+}
+
+func oneSecondTick(size *int64, r *http.Response) {
+	ticker := time.NewTicker(time.Second)
+
+	for range ticker.C {
+		fmt.Printf("\r[%s] %.2f%% of %d bytes", progressBar(*size, r.ContentLength), float64(*size)/float64(r.ContentLength)*100, r.ContentLength)
+	}
+}
